b6044b9c5ead2f18831897f9275dd9d66d790e83
Jono <jonomacgregor@gmail.com>
2020-06-08 14:49:09 +1200

    Adding Restful Service

---
+++ MovieSelfHost/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MovieSelfHost
+{
+    class MovieController : System.Web.Http.ApiController
+    {
+    public List<string> GetGenreNames()
+    { 
+            DataTable lcResult = clsDbConnection.GetDataTable("SELECT Name FROM Genre", null);
+            List<string> lcNames = new List<string>();
+            foreach (DataRow dr in lcResult.Rows) 
+                lcNames.Add((string)dr[0]);
+            return lcNames; }
+    }
+}
--- MovieSelfHost/clsDbConnection.cs
+++ MovieSelfHost/clsDbConnection.cs
@@ -22,26 +22,44 @@ namespace MovieSelfHost
             // for SELECT 
             using (DataTable lcDataTable = new DataTable("TheTable")) 
             using (DbConnection lcDataConnection = ProviderFactory.CreateConnection()) 
-            using (DbCommand lcCommand = lcDataConnection.CreateCommand()) { lcDataConnection.ConnectionString = ConnectionStr;
+            using (DbCommand lcCommand = lcDataConnection.CreateCommand())
+            { 
+                lcDataConnection.ConnectionString = ConnectionStr;
                 lcDataConnection.Open();
                 lcCommand.CommandText = prSQL;
                 setPars(lcCommand, prPars);
-                using (DbDataReader lcDataReader = lcCommand.ExecuteReader(CommandBehavior.CloseConnection)) lcDataTable.Load(lcDataReader);
+                using (DbDataReader lcDataReader = lcCommand.ExecuteReader(CommandBehavior.CloseConnection))
+                    lcDataTable.Load(lcDataReader);
                 return lcDataTable; 
-            } } public static int Execute(string prSQL, Dictionary<string, Object> prPars) { // for UPDATE, INSERT, DELETE 
+            } 
+        } 
+        public static int Execute(string prSQL, Dictionary<string, Object> prPars) 
+        { 
+            // for UPDATE, INSERT, DELETE 
             using (DbConnection lcDataConnection = ProviderFactory.CreateConnection()) 
-            using (DbCommand lcCommand = lcDataConnection.CreateCommand()) { lcDataConnection.ConnectionString = ConnectionStr; 
+            using (DbCommand lcCommand = lcDataConnection.CreateCommand()) 
+            { 
+                lcDataConnection.ConnectionString = ConnectionStr; 
                 lcDataConnection.Open(); lcCommand.CommandText = prSQL;
-                setPars(lcCommand, prPars); return lcCommand.ExecuteNonQuery(); 
-            } } private static void setPars(DbCommand prCommand, Dictionary<string, Object> prPars) { // For most DBMS 
-            using @Name1, @Name2, @Name3 etc. 
-                if (prPars != null) foreach (KeyValuePair<string, Object> lcItem in prPars) { DbParameter lcPar = ProviderFactory.CreateParameter(); 
+                setPars(lcCommand, prPars); 
+                return lcCommand.ExecuteNonQuery(); 
+            } 
+        } 
+        private static void setPars(DbCommand prCommand, Dictionary<string, Object> prPars) 
+        { 
+            // For most DBMS using @Name1, @Name2, @Name3 etc. 
+                if (prPars != null)
+                    foreach (KeyValuePair<string, Object> lcItem in prPars)
+                { 
+                    DbParameter lcPar = ProviderFactory.CreateParameter(); 
                     lcPar.Value = lcItem.Value == null ? DBNull.Value : lcItem.Value; 
                     lcPar.ParameterName = '@' + lcItem.Key;
                     prCommand.Parameters.Add(lcPar); 
                 } 
         }
 
-        }
     }
 }
+
+
+
